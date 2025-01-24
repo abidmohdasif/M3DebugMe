e0044beffc503bc775a72ea666d85a00620152c1
Abid Asif <amasif@my.waketech.edu>
2025-01-24 12:39:46 -0500

    DEBUG

---
+++ Debug/Me/trouble.js
@@ -0,0 +1,46 @@
+let v1 = 3;
+let v2 = 4
+alert("3 times 4" + " equals " + v1 * v2); // Uncommented the alert and equals needed quotations
+
+ // This sets the general styling for the divs within the section tag using .style
+            let section = document.getElementById("s1") // Got rid of the "s" at the end of
+            section.style.width = "50%"
+            section.style.textAlign = "center"
+            section.style.fontFamily = "arial, serif"
+            section.style.fontWeight = "bold" // Corrected the Spelling for "style"
+            section.style.fontStyle = "italic"
+            section.style.fontSize = "20px"
+
+            // Sets the customization for each individual color/div tag
+			//.innerHTML is used to add content within each div tag.
+			//.style.backgroundColor is used to assign a different background color to each div
+			
+            // RED
+            const red = document.getElementById("red")  //Capatalize the "e"
+            red.innerHTML = "RED"
+            red.style.backgroundColor = "red"
+
+            // ORANGE
+            const orange = document.getElementById("orange")
+            orange.innerHTML = "ORANGE" // Capatalize the "html"
+            orange.style.backgroundColor = "orange"
+
+            // YELLOW
+            const yellow = document.getElementById("yellow")
+            yellow.innerHTML = "YELLOW"
+            yellow.style.backgroundColor = "yellow"
+
+            // GREEN
+            const green = document.getElementById("green") // Change the color from purple to green
+            green.innerHTML = "GREEN"
+            green.style.backgroundColor = "green"
+
+            // INDIGO
+            const indigo = document.getElementById("indigo")
+            indigo.innerHTML = "INDIGO"
+            indigo.style.backgroundColor = "indigo"
+
+            // PURPLE
+            const violet = document.getElementById("violet")
+            violet.innerHTML = "VIOLET"
+            violet.style.backgroundColor = "violet"
